cc4b2a6777f3c2748e6bda93c0d6a336f0db4f93
proco-guru <ankita.k.pawar1906@gmail.com>
2026-06-20 15:45:44 +0530

    Day6: dependency Injection and Services

--- src/app/components/transaction-history/transaction-history.ts
+++ src/app/components/transaction-history/transaction-history.ts
@@ -1,16 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-//define txnType
-interface Transaction {
-  txnId: string;
-  date: Date;
-  description: string;
-  type: 'CREDIT' | 'DEBIT';
-  amount: number;
-  balanceAfter: number;
-  status: 'SUCCESS' | 'PENDING' | 'FAILED';
-}
+import { Transaction, TransactionService } from '../../services/transaction-service';
+
 @Component({
   selector: 'app-transaction-history',
   imports: [FormsModule, CommonModule],
@@ -18,64 +10,24 @@ interface Transaction {
   styleUrl: './transaction-history.css',
 })
 export class TransactionHistory {
-  selectedType: 'All' | 'CREDIT' | 'DEBIT' = 'All'; //setting fix reqiured types, and default =ALL
+  transactions!: Transaction[];
+  //append dummy data for now
+  txnDetail: Transaction = {
+    txnId: 1007,
+    date: new Date(),
+    description: 'Grocery Payment Debited',
+    type: 'DEBIT',
+    amount: 26500,
+    balanceAfter: 336749,
+    status: 'SUCCESS',
+  };
 
-  transactions: Transaction[] = [
-    {
-      txnId: 'TXN1001',
-      date: new Date('2026-06-15'),
-      description: 'Salary Credit - Infosys Ltd',
-      type: 'CREDIT',
-      amount: 85000,
-      balanceAfter: 425750,
-      status: 'SUCCESS',
-    },
-    {
-      txnId: 'TXN1002',
-      date: new Date('2026-06-14'),
-      description: 'Electricity Bill - MSEB',
-      type: 'DEBIT',
-      amount: 2400,
-      balanceAfter: 340750,
-      status: 'SUCCESS',
-    },
-    {
-      txnId: 'TXN1003',
-      date: new Date('2026-06-13'),
-      description: 'NEFT to Ramesh Kumar',
-      type: 'DEBIT',
-      amount: 15000,
-      balanceAfter: 343150,
-      status: 'PENDING',
-    },
-    {
-      txnId: 'TXN1004',
-      date: new Date('2026-06-12'),
-      description: 'Mobile Recharge - Jio',
-      type: 'DEBIT',
-      amount: 599,
-      balanceAfter: 358150,
-      status: 'FAILED',
-    },
-    {
-      txnId: 'TXN1005',
-      date: new Date('2026-06-10'),
-      description: 'Freelance Payment Received',
-      type: 'CREDIT',
-      amount: 22000,
-      balanceAfter: 358749,
-      status: 'SUCCESS',
-    },
-    {
-      txnId: 'TXN1006',
-      date: new Date('2026-06-15'),
-      description: 'Cloth Payment Debited',
-      type: 'DEBIT',
-      amount: 22000,
-      balanceAfter: 336749,
-      status: 'SUCCESS',
-    },
-  ];
+  constructor(private transactionService: TransactionService) {}
+  ngOnInit() {
+    this.transactions = this.transactionService.getTransactions();
+  }
+
+  selectedType: 'All' | 'CREDIT' | 'DEBIT' = 'All'; //setting fix reqiured types, and default =ALL
 
   //using getter prop to :filter txnData and return based on TxnType
   get filteredTransactions(): Transaction[] {
@@ -86,7 +38,14 @@ export class TransactionHistory {
   }
 
   // Add this method to the class
-  trackByTxnId(index: number, txn: Transaction): string {
+  trackByTxnId(index: number, txn: Transaction): number {
     return txn.txnId;
+  }
+
+  //method is to add/append new txn history record
+  addTxnDetails(): void {
+    //to append txn
+    this.transactionService.addTransaction(this.txnDetail);
+    this.transactions = this.transactionService.getTransactions(); //to get updated/latest txns record
   }
 }
---
+++ src/app/services/transaction-service.ts
@@ -0,0 +1,84 @@
+import { Injectable } from '@angular/core';
+
+//define txnType
+export interface Transaction {
+  txnId: number;
+  date: Date;
+  description: string;
+  type: 'CREDIT' | 'DEBIT';
+  amount: number;
+  balanceAfter: number;
+  status: 'SUCCESS' | 'PENDING' | 'FAILED';
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TransactionService {
+  private transactions: Transaction[] = [
+    {
+      txnId: 1001,
+      date: new Date('2026-06-15'),
+      description: 'Salary Credit - Infosys Ltd',
+      type: 'CREDIT',
+      amount: 85000,
+      balanceAfter: 425750,
+      status: 'SUCCESS',
+    },
+    {
+      txnId: 1002,
+      date: new Date('2026-06-14'),
+      description: 'Electricity Bill - MSEB',
+      type: 'DEBIT',
+      amount: 2400,
+      balanceAfter: 340750,
+      status: 'SUCCESS',
+    },
+    {
+      txnId: 1003,
+      date: new Date('2026-06-13'),
+      description: 'NEFT to Ramesh Kumar',
+      type: 'DEBIT',
+      amount: 15000,
+      balanceAfter: 343150,
+      status: 'PENDING',
+    },
+    {
+      txnId: 1004,
+      date: new Date('2026-06-12'),
+      description: 'Mobile Recharge - Jio',
+      type: 'DEBIT',
+      amount: 599,
+      balanceAfter: 358150,
+      status: 'FAILED',
+    },
+    {
+      txnId: 1005,
+      date: new Date('2026-06-10'),
+      description: 'Freelance Payment Received',
+      type: 'CREDIT',
+      amount: 22000,
+      balanceAfter: 358749,
+      status: 'SUCCESS',
+    },
+    {
+      txnId: 1006,
+      date: new Date('2026-06-15'),
+      description: 'Cloth Payment Debited',
+      type: 'DEBIT',
+      amount: 22000,
+      balanceAfter: 336749,
+      status: 'SUCCESS',
+    },
+  ];
+
+  //retrun the copy of txn array
+  getTransactions(): Transaction[] {
+    return [...this.transactions];
+  }
+
+  //update the trxn array using immutable form
+  addTransaction(txn: Transaction): void {
+    this.transactions = [...this.transactions, txn];
+  }
+}
